d8cdde7283c455e75f38bddca866bf9e03c7bf54
Navendu-cyber <mr2005pc3779@gmail.com>
2025-09-08 12:50:13 +0530

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,20 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:test/screens/homescreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TodoApp());
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DogApiScreen(),
-    );
-  }
-}
-
-class DogApiScreen extends StatefulWidget {
-  const DogApiScreen({super.key});
-
-  @override
-  State<DogApiScreen> createState() => _DogApiScreenState();
-}
-
-class _DogApiScreenState extends State<DogApiScreen> {
-  String? dogImageUrl; // to store the image URL
-  bool isLoading = false;
-
-  // Fetch random dog image
-  Future<void> fetchDogImage() async {
-    setState(() => isLoading = true);
-
-    final url = Uri.parse('https://dog.ceo/api/breeds/image/random');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      setState(() {
-        dogImageUrl = data['message']; // "message" contains the image URL
-        isLoading = false;
-      });
-    } else {
-      setState(() => isLoading = false);
-      throw Exception('Failed to load dog image');
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchDogImage(); // load one image when screen starts
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Random Dog API")),
-      body: Center(
-        child: isLoading
-            ? const CircularProgressIndicator()
-            : dogImageUrl == null
-            ? const Text("No image loaded")
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.network(
-                    dogImageUrl!,
-                    height: 300,
-                    width: 300,
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: fetchDogImage,
-                    child: const Text("Fetch Another Dog 🐶"),
-                  ),
-                ],
-              ),
-      ),
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Homescreen(),
     );
   }
 }
---
+++ lib/modules/student/models/student_model.dart
@@ -0,0 +1,6 @@
+class StudentModel {
+  final String name;
+  final int age;
+  final int studentid;
+  StudentModel(this.name, this.age, this.studentid, {required});
+}
